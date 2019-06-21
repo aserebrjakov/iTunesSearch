2e41bfd4d16fd0fa6c168bbedb42961c2d10dfb1
Alexandr Serebrjakov <tac.serebrjakov.a@gmail.com>
2019-06-21 16:59:35 +0300

    add UIStackView

--- iTunesSearchSwift/Album/AlbumHeaderCell.swift
+++ iTunesSearchSwift/Album/AlbumHeaderCell.swift
@@ -35,61 +35,61 @@ import UIKit
 
 class AlbumHeaderCell: UITableViewCell {
     
-    var collectionNameLabel: UILabel = UILabel(frame: CGRect.zero)
-    var artistNameLabel: UILabel = UILabel(frame: CGRect.zero)
-    var genreLabel: UILabel = UILabel(frame: CGRect.zero)
-    var countryLabel: UILabel = UILabel(frame: CGRect.zero)
-    var releaseDateLabel: UILabel = UILabel(frame: CGRect.zero)
     var artworkImageView: UIImageView = UIImageView(frame: CGRect.zero)
+    var stackView: UIStackView = UIStackView()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String!) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.selectionStyle = .none
         self.addImageView(&artworkImageView)
-        self.addLabel(label: &collectionNameLabel, highLabel: nil)
-        self.addLabel(label: &artistNameLabel, highLabel: collectionNameLabel)
-        self.addLabel(label: &genreLabel, highLabel: artistNameLabel)
-        self.addLabel(label: &countryLabel, highLabel: genreLabel)
-        self.addLabel(label: &releaseDateLabel, highLabel: countryLabel)
+        self.addStackView(stack: &stackView)
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-
+    
     func addImageView(_ imageView:inout UIImageView) {
         imageView.translatesAutoresizingMaskIntoConstraints = false;
+        
         contentView.addSubview(imageView)
+        
         imageView.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 10).isActive = true
         imageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8).isActive = true
         contentView.bottomAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8).isActive = true
         imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor, multiplier: 1).isActive = true
     }
     
-    func addLabel(label:inout UILabel, highLabel:UILabel!) {
-        label.translatesAutoresizingMaskIntoConstraints = false;
-        contentView.addSubview(label)
-        label.leftAnchor.constraint(equalTo: artworkImageView.rightAnchor, constant: 15).isActive = true
-        if highLabel != nil {
-            label.topAnchor.constraint(equalTo: highLabel.bottomAnchor, constant: 8).isActive = true
-        } else {
-            label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8).isActive = true
-        }
+    func addStackView(stack:inout UIStackView) {
+        stack.axis  = NSLayoutConstraint.Axis.vertical
+        stack.distribution  = UIStackView.Distribution.fillProportionally
+        stack.alignment = UIStackView.Alignment.leading
+        stack.translatesAutoresizingMaskIntoConstraints = false
         
+        contentView.addSubview(stack)
+        
+        stack.leftAnchor.constraint(equalTo: artworkImageView.rightAnchor, constant: 15).isActive = true
+        stack.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: 15).isActive = true
+        stack.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: stack.bottomAnchor, constant: 8).isActive = true
+    }
+    
+    
+    func titleLabel(name:String, string:String!) -> UILabel {
+        let label = UILabel(frame: CGRect.zero)
+        label.translatesAutoresizingMaskIntoConstraints = false;
+        let text = Utils.twoWord(name:name, string:string)
+        label.attributedText = text
+        return label
     }
     
     func configureCell(item: iTunesItem!) {
-        let collection = Utils.twoWord(name:"Альбом:", string:item.collectionName!)
-        let artistName = Utils.twoWord(name:"Артист:", string:item.artistName!)
-        let genre = Utils.twoWord(name:"Жанр:", string:item.genreName!)
-        let country = Utils.twoWord(name:"Страна:", string:item.country!)
-        let releaseDate = Utils.twoWord(name:"Год:", string:item.year)
         
-        collectionNameLabel.attributedText = collection
-        artistNameLabel.attributedText = artistName
-        genreLabel.attributedText = genre
-        countryLabel.attributedText = country
-        releaseDateLabel.attributedText = releaseDate
+        stackView.addArrangedSubview(titleLabel(name:"Альбом:", string:item.collectionName!))
+        stackView.addArrangedSubview(titleLabel(name:"Артист:", string:item.artistName!))
+        stackView.addArrangedSubview(titleLabel(name:"Жанр:", string:item.genreName!))
+        stackView.addArrangedSubview(titleLabel(name:"Страна:", string:item.country!))
+        stackView.addArrangedSubview(titleLabel(name:"Год:", string:item.year))
         
         self.artworkImageView.image = UIImage(named:"noArtwork")
         ImageManager.download(path: item.artworkUrl600) { (image) in
