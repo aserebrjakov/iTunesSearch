59f1c87c6d077abcbbed6aaa76d9451443896b2c
Alexandr Serebrjakov <tac.serebrjakov.a@gmail.com>
2019-07-22 18:32:57 +0300

    move files in project, create API group

---
+++ iTunesSearchSwift/API/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  iTunesSearchSwift
+//
+//  Created by Alexandr on 22/07/2019.
+//  Copyright © 2019 iMac. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager: NSObject {
+    
+}
